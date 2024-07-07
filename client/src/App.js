ef41308be6532ad960d0607147ec6ccc40053d6e
AnuruddhSin <anuruddh7234@gmail.com>
2024-07-08 02:01:53 +0530

    Adding UploadVideo in the dirc UploadVideo and  route of the file is in TeacherRoutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import NewRoute from './Routes/NewRoute';
 import AdminRoute from './Routes/AdminRoute';
+import StudentRoute from './Routes/StudentRoutes';
+import TeacherRoutes from './Routes/TeacherRoutes';
 
 function App() {
   return (
     <div>
       <NewRoute/>
       <AdminRoute/>
+      <StudentRoute/>
+      <TeacherRoutes/>
     </div>
   )
 }
--- client/src/Routes/NewRoute.js
+++ client/src/Routes/NewRoute.js
@@ -10,7 +10,6 @@ import Customer from '../Admin/components/Customer/Customer';
 import Orders from '../Admin/components/Orders/OrdersTable';
 import Products from '../Admin/components/Products/ProductsTable';
 import createProduct from "../Admin/components/createProduct/CreateProductForm";
-// import YourProfile from '../Profile/Components/YourProfile/YourProfile';
 import Usercources from "../components/userComponent/MyCources/MyCources";
 import Teachercources from "../components/userComponent/MyCources/MyCources";
 
@@ -28,7 +27,6 @@ function NewRoute() {
         <Route path="/admin/orders" element={<Orders />} />
         <Route path="/admin/products" element={<Products />} />
         <Route path="/admin/createProduct" element={<createProduct />} />
-        {/* <Route path="/profile" element={<YourProfile />} /> */}
         <Route path="/user/courses" element={<Usercources />} />
         <Route path="/teacher/courses" element={<Teachercources />} />
       </Routes>
