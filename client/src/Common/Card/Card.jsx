7c3bb874e5e11c1101811b030dfd0d469eec205b
code-sharad <codesharad@gmail.com>
2024-07-11 16:05:32 +0530

    Modified the Navbar and Added Login an d Register functionality

--- client/src/Common/Card/Card.jsx
+++ client/src/Common/Card/Card.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function Card({ title, author, description }) {
     return (
-        <Link to={`#`} className='w-[320px] text-white border border-[#1d1b22]  bg-black hover:scale-[103%] transition-all  font-inter  overflow-hidden mt-2   rounded-xl'>
-            <img className='w-full h-full' alt='course' src={`https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/41/4d3d7c05fb42729c9d90352e072ca3/1060x596_GCC-photos_Karrim.png?auto=format%2Ccompress%2C%20enhance&dpr=1&w=320&h=180&fit=crop&q=50&crop=faces `} />
-            <section className='flex my-2 flex-col gap-2 p-4'>
+        <Link to={`#`} className='w-[320px] h-[400px] text-white border border-[#1d1b22]  bg-black hover:scale-[103%] transition-all  font-inter  overflow-hidden mt-2   rounded-xl'>
+            <img className='w-[320px] h-[190px]' alt='course' src={`https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/41/4d3d7c05fb42729c9d90352e072ca3/1060x596_GCC-photos_Karrim.png?auto=format%2Ccompress%2C%20enhance&dpr=1&w=320&h=180&fit=crop&q=50&crop=faces `} />
+            <section className='flex my-2 w-[320px] flex-col gap-2 p-4'>
                 <h1 className='font-bold text-lg  '>Full Stack Web Development</h1>
                 <p className='text-gray-400  text-md font-semibold'>Vivek Shejole</p>
                 <p className='tracking-wide'>Ready to rule the digital world. Learn to build powerfull fullstack
