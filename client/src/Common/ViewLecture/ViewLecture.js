6d194d58ad807ade7a3a0b34ccc033be9ea454f6
sushantbhausahebwalunj <sushantwalunj2001@gmail.com>
2024-07-13 10:53:11 +0530

    modified viewlecture component added subsections

--- client/src/Common/ViewLecture/ViewLecture.js
+++ client/src/Common/ViewLecture/ViewLecture.js
@@ -1,6 +1,9 @@
-
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
+import Comment from "../../Tabs/Comment";
+import Attachment from "../../Tabs/Attachment";
+import Doubts from "../../Tabs/Doubts";
+import Refrence from "../../Tabs/Refrence";
 
 function ViewLecture() {
   const dummyData = [
@@ -9,61 +12,62 @@ function ViewLecture() {
       description: "this is a very useful video",
       channel: "sushant01",
       thumbnail: "./logo192.png",
-      url: "https://youtu.be/YDH7f9dTXAs?si=a76QL1swf1dLRYmv" 
+      url: "https://youtu.be/YDH7f9dTXAs?si=a76QL1swf1dLRYmv",
     },
     {
       title: "video2",
       description: "this is a very very useful video",
       channel: "sushant01",
       thumbnail: "./logo192.png",
-      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
+      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
     },
     {
-        title: "video2",
-        description: "this is a very very useful video",
-        channel: "sushant01",
-        thumbnail: "./logo192.png",
-        url: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
-      },
-      {
-        title: "video2",
-        description: "this is a very very useful video",
-        channel: "sushant01",
-        thumbnail: "./logo192.png",
-        url: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
-      },
-      {
-        title: "video2",
-        description: "this is a very very useful video",
-        channel: "sushant01",
-        thumbnail: "./logo192.png",
-        url: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
-      },
-      {
-        title: "video2",
-        description: "this is a very very useful video",
-        channel: "sushant01",
-        thumbnail: "./logo192.png",
-        url: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
-      },
-      {
-        title: "video2",
-        description: "this is a very very useful video",
-        channel: "sushant01",
-        thumbnail: "./logo192.png",
-        url: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
-      },
-      {
-        title: "video2",
-        description: "this is a very very useful video",
-        channel: "sushant01",
-        thumbnail: "./logo192.png",
-        url: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
-      },
+      title: "video2",
+      description: "this is a very very useful video",
+      channel: "sushant01",
+      thumbnail: "./logo192.png",
+      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
+    },
+    {
+      title: "video2",
+      description: "this is a very very useful video",
+      channel: "sushant01",
+      thumbnail: "./logo192.png",
+      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
+    },
+    {
+      title: "video2",
+      description: "this is a very very useful video",
+      channel: "sushant01",
+      thumbnail: "./logo192.png",
+      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
+    },
+    {
+      title: "video2",
+      description: "this is a very very useful video",
+      channel: "sushant01",
+      thumbnail: "./logo192.png",
+      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
+    },
+    {
+      title: "video2",
+      description: "this is a very very useful video",
+      channel: "sushant01",
+      thumbnail: "./logo192.png",
+      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
+    },
+    {
+      title: "video2",
+      description: "this is a very very useful video",
+      channel: "sushant01",
+      thumbnail: "./logo192.png",
+      url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
+    },
   ];
 
   const [activeVideo, setActiveVideo] = useState(null);
   const [playing, setPlaying] = useState(false);
+  const [activeTab, setActiveTab] = useState("");
 
   const playVideo = (video) => {
     setActiveVideo(video);
@@ -77,40 +81,113 @@ function ViewLecture() {
   const handleSeek = (seconds) => {
     setPlaying(false);
     setTimeout(() => setPlaying(true), 100);
-    activeVideo.ref.seekTo(seconds, 'seconds');
+    activeVideo.ref.seekTo(seconds, "seconds");
+  };
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
   };
 
   return (
     <div className="flex flex-col md:flex-row">
-
-{activeVideo && (
+      {activeVideo && (
         <div className="w-full  md:w-4/4  flex flex-col items-center bg-gray-900 p-5  ">
           <ReactPlayer
-            ref={(player) => { activeVideo.ref = player; }}
+            ref={(player) => {
+              activeVideo.ref = player;
+            }}
             url={activeVideo.url}
             playing={playing}
             controls
             width="70vw"
             height="90vh"
+            className="max-h-[90vh]"
           />
           <div className="mt-5 flex space-x-4">
-            <button onClick={handlePlayPause} className="px-4 py-2 bg-blue-600 text-white rounded">
+            <button
+              onClick={handlePlayPause}
+              className="px-4 py-2 bg-blue-600 text-white rounded"
+            >
               {playing ? "Pause" : "Play"}
             </button>
-            <button onClick={() => handleSeek(activeVideo.ref.getCurrentTime() - 5)} className="px-4 py-2 bg-blue-600 text-white rounded">
+            <button
+              onClick={() => handleSeek(activeVideo.ref.getCurrentTime() - 5)}
+              className="px-4 py-2 bg-blue-600 text-white rounded"
+            >
               -5s
             </button>
-            <button onClick={() => handleSeek(activeVideo.ref.getCurrentTime() + 5)} className="px-4 py-2 bg-blue-600 text-white rounded">
+            <button
+              onClick={() => handleSeek(activeVideo.ref.getCurrentTime() + 5)}
+              className="px-4 py-2 bg-blue-600 text-white rounded"
+            >
               +5s
             </button>
             
           </div>
+
+          <ul className="text-white flex gap-5 py-10 ">
+            <li>
+              <a className="" href="#">
+                <button
+                  className={`w-full flex items-center  capitalize  `}
+                  type="button"
+                  onClick={() => handleTabChange("comments")}
+                >
+                  <li className="p-2 cursor-pointer tracking-wider hover:text-slate-300">Comments</li>
+                </button>
+              </a>
+            </li>
+            <li>
+              <a className="" href="#">
+                <button
+                  className={`w-full flex items-center  capitalize `}
+                  type="button"
+                  onClick={() => handleTabChange("attachments")}
+                >
+                  <li className="p-2 cursor-pointer tracking-wider hover:text-slate-300">attachments</li>
+                </button>
+              </a>
+            </li>
+            <li>
+              <a className="" href="#">
+                <button
+                  className={`w-full flex items-center  capitalize `}
+                  type="button"
+                  onClick={() => handleTabChange("doubts")}
+                >
+                  <li className="p-2 cursor-pointer tracking-wider hover:text-slate-300">doubts</li>
+                </button>
+              </a>
+            </li>
+            <li>
+              <a className="" href="#">
+                <button
+                  className={`w-full flex items-center  capitalize `}
+                  type="button"
+                  onClick={() => handleTabChange("refrence")}
+                >
+                  <li className="p-2 cursor-pointer tracking-wider hover:text-slate-300">refrence</li>
+                </button>
+              </a>
+            </li>
+          </ul>
+
+          <div className="">
+        
+        {activeTab === "comments" && <Comment />}
+        {activeTab === "attachments" && <Attachment />}
+        {activeTab === "doubts" && <Doubts />}
+        {activeTab === "refrence" && <Refrence />}
+
+      </div>
         </div>
       )}
 
+ 
 
-
-      <ul className="space-y-5 py-5 mx-auto md:w-1/4" style={{ width: "90vw", maxWidth: "60vw" }}>
+      <ul
+        className="space-y-5 py-5 mx-auto md:w-1/4"
+        style={{ width: "90vw", maxWidth: "60vw" }}
+      >
         {dummyData.map((data, i) => (
           <li
             key={i}
@@ -126,8 +203,12 @@ function ViewLecture() {
               </button>
             </div>
             <div className="text-center md:text-left">
-              <h3 className="text-xl md:text-2xl font-medium mb-2 text-white">{data.title}</h3>
-              <h5 className="text-gray-100 mb-2 text-lg md:text-xl">{data.description}</h5>
+              <h3 className="text-xl md:text-2xl font-medium mb-2 text-white">
+                {data.title}
+              </h3>
+              <h5 className="text-gray-100 mb-2 text-lg md:text-xl">
+                {data.description}
+              </h5>
               <p className="text-gray-500">{data.channel}</p>
             </div>
           </li>
